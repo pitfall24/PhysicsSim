479c439d1d42860d60422e9ce42fedbda0e1b81a
Phineas Scovel <62225152+pitfall24@users.noreply.github.com>
2022-04-28 11:02:22 -0400

    added stuff

--- driver.py
+++ driver.py
@@ -1,10 +1,14 @@
 class Object:
-  def __init__(self, posx, posy, mass, volume, static = False):
+  def __init__(self, posx, posy, mass, volume, static = False, name = None):
+    self.name = name
+    
     self.posx = posx
     self.posy = posy
     
     self.velx = 0
     self.vely = 0
+    self.accx = 0
+    self.accy = 0
     
     self.mass = mass
     self.volume = volume
@@ -20,6 +24,10 @@ class Object:
     self.posy = posy
     self.posx = posx
   
+  def setacc(accx, accy):
+    self.accx = accx
+    self.accy = accy
+  
   def updatevel(accx, accy):
     self.forcex = mass / accx
     self.forcey = mass / accy
@@ -33,3 +41,10 @@ class Object:
     
     posx += velx
     posy += vely
+    
+  def printinfo():
+    print('Name:', name)
+    print('Position (x, y): (' + str(posx) + ', ' + str(posy) + ')')
+    print('Velocity (x, y): (' + str(velx) + ', ' + str(vely) + ')')
+    print('Acceleration (x, y): (' + str(accx) + ', ' + str(accy) + ')')
+    print('Mass (kg):', mass + 'kg, Volume (m^3):', volume + 'm^3')
