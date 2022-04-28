aab44f84b1fc09a7b1b390abfe16effe18300463
Phineas Scovel <62225152+pitfall24@users.noreply.github.com>
2022-04-28 11:22:09 -0400

    made test

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-class Object:
-  def __init__(self, posx, posy, mass, volume, static = False):
-    self.posx = posx
-    self.posy = posy
-    
-    self.velx = 0
-    self.vely = 0
-    
-    self.mass = mass
-    self.volume = volume
-    self.density = mass / volume
-    
-    self.static = static
-    
-  def setvel(velx, vely):
-    self.velx = velx
-    self.vely = vely
-    
-  def setpos(posy, posx):
-    self.posy = posy
-    self.posx = posx
+from driver.py import *
+
+firstObject = Object(0, 0, 10, 4, name = 'Test Object')
+
+firstObject.printinfo()
+
+firstObject.setpos(10, 10)
+firstObject.setvel(3, 3)
+for _ in range(3):
+  firstObject.printinfo()
+  firstobject.updatepos()
   
-  def updatevel(accx, accy):
-    self.forcex = mass / accx
-    self.forcey = mass / accy
-    
-    velx += accx
-    vely += accy
-    
-  def updatepos():
-    if not static:
-      return
-    
-    posx += velx
-    posy += vely
+firstObject.printinfo()
+firstObject.setacc(-0.5, -0.5)
+
+for _ in range(5):
+  firstObject.printinfo()
+  firstObject.updatevel()
+  firstObject.updatepos()
+  
+firstObject.printinfo()
