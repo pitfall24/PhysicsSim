cde3395b04ae90286aac235ac45cbfc22710cbec
Phineas Scovel <62225152+pitfall24@users.noreply.github.com>
2022-04-28 12:10:20 -0400

    added calc attraction and force to acc, fixed some stuff?

--- driver.py
+++ driver.py
@@ -1,3 +1,7 @@
+from math import sqrt, sin, cos, arctan
+
+GRAVITATION_CONSTANT = 6.6743e-11
+
 class Object:
   def __init__(self, posx, posy, mass, volume, static = False, name = None):
     self.name = name
@@ -15,36 +19,56 @@ class Object:
     self.density = mass / volume
     
     self.static = static
-    
-  def setvel(velx, vely):
-    self.velx = velx
-    self.vely = vely
-    
-  def setpos(posy, posx):
-    self.posy = posy
-    self.posx = posx
   
-  def setacc(accx, accy):
-    self.accx = accx
-    self.accy = accy
+  def setvel(newvelx, newvely):
+    self.velx = newvelx
+    self.vely = newvely
+    
+  def setpos(newposy, newposx):
+    self.posy = newposy
+    self.posx = newposx
+  
+  def setacc(newaccx, newaccy):
+    self.accx = newaccx
+    self.accy = newaccy
   
   def updatevel(accx, accy):
-    self.forcex = mass / accx
-    self.forcey = mass / accy
+    self.forcex = self.mass / self.accx
+    self.forcey = self.mass / self.accy
     
-    velx += accx
-    vely += accy
+    self.velx += self.accx
+    self.vely += self.accy
     
   def updatepos():
-    if not static:
+    if not self.static:
       return
-    
-    posx += velx
-    posy += vely
+    else:
+      self.posx += self.velx
+      self.posy += self.vely
     
   def printinfo():
-    print('Name:', name)
-    print('Position (x, y): (' + str(posx) + ', ' + str(posy) + ')')
-    print('Velocity (x, y): (' + str(velx) + ', ' + str(vely) + ')')
-    print('Acceleration (x, y): (' + str(accx) + ', ' + str(accy) + ')')
-    print('Mass (kg):', mass + 'kg, Volume (m^3):', volume + 'm^3')
+    print('Name:', self.name)
+    print('Position (x, y): (' + str(self.posx) + ', ' + str(self.posy) + ')')
+    print('Velocity (x, y): (' + str(self.velx) + ', ' + str(self.vely) + ')')
+    print('Acceleration (x, y): (' + str(self.accx) + ', ' + str(self.accy) + ')')
+    print('Mass (kg):', self.mass + 'kg, Volume (m^3):', self.volume + 'm^3')
+
+def calcattraction(object1, object2):
+    relx = object1.posx - object2.posx
+    rely = object1.posy - object2.posy
+    mass1, mass2 = object1.mass, object2.mass
+    
+    distance = sqrt(relx ** 2 + rely ** 2)
+    force = GRAVITATIONAL_CONSTANT * mass1 * mass2 / distance ** 2
+    angle = arctan(rely / relx)
+    
+    attractionx = cos(angle) * force
+    attractiony = sin(angle) * force
+    
+    return attractionx, attractiony
+
+def forcetoacc(mass, forcex, forcey):
+  accx = forcex / mass
+  accy = forcey / mass
+  
+  return accx, accy
