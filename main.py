78eeac9bdaf55bcd32e53501f15bac9a4c5d254d
Phineas Scovel <pscovel24@gmail.com>
2022-05-05 23:27:02 -0400

    idk did some cool stuff i guess

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ from matplotlib.animation import FuncAnimation
 
 from driver import *
 
-GLOBAL_FPS = 60
+GLOBAL_FPS = 24
 
 firstObject = Object(0, 0, 0, 0, name = 'First Object', fps = GLOBAL_FPS)
 secondObject = Object(0, 0, 0, 0, name = 'Second Object', fps = GLOBAL_FPS)
 thirdObject = Object(0, 0, 0, 0, name = 'Third Object', fps = GLOBAL_FPS)
 fourthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+fifthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+sixthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+seventhObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+eigthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+ninthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
+tenthObject = Object(0, 0, 0, 0, name = 'Fourth Object', fps = GLOBAL_FPS)
 
-testSystem = System(GLOBAL_FPS, 'Test System', firstObject, secondObject, thirdObject, fourthObject)
+testSystem = System(GLOBAL_FPS, 'Test System', firstObject, secondObject, thirdObject, fourthObject, fifthObject, sixthObject, seventhObject, eigthObject, ninthObject, tenthObject)
 testSystem.setacc_all(0, -9.8)
 
-testSystem.setvel_list([1, 1, 1, 1], [12, 8, 6, 4])
-testSystem.setpos_list([0, 0, 0, 0], [0, -1, -2, -3])
+testSystem.setvel_list([1 for _ in range(10)], [12 - i for i in range(10)])
+testSystem.setpos_list([0 for _ in range(10)], [-i / 3 for i in range(10)])
 
 coords = []
+runtime = 2.5
 
-while testSystem.objects[0].posy > -3:
+while len(coords) < GLOBAL_FPS * len(testSystem.objects) * runtime:
   testSystem.updateposandvel()
   coords += testSystem.returninfo()
 
@@ -34,5 +41,5 @@ def animate(i):
     graph.set_data(x[:i+1], y[:i+1])
     return graph
 
-ani = FuncAnimation(fig, animate, frames = len(coords), interval = firstObject.dt * 1000)
+ani = FuncAnimation(fig, animate, frames = len(coords), interval = 1000 / (GLOBAL_FPS * len(testSystem.objects)))
 plt.show()
